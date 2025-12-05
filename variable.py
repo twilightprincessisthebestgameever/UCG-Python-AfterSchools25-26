c9c69623b136318b9bcfec4dbac9f2cb7ed83821
twilightprincessisthebestgameever <iluvskywardsword@gmail.com>
2025-12-04 19:21:54 -0600

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,8 @@
+# Program Description : This is a self-intro program
+#Varibles that store name, age and favorite food
+name = "Maximillian"
+age = 13
+favfood = "Donut"
+
+#Displaying all the information stored to user. 
+print(f"My name is {name}. I am {age} years old. My favorite food is {favfood}." )
