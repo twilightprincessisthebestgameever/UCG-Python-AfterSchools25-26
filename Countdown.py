5089b5d3d26bdd0fd2f85c87a450aa7d0d51ac14
twilightprincessisthebestgameever <iluvskywardsword@gmail.com>
2025-12-04 19:12:03 -0600

    Create Countdown.py

---
+++ Countdown.py
@@ -0,0 +1,17 @@
+# Countdown Timer Program
+#Program Description: This program counts down from a user-given number to 1.
+
+#importing time module to add 1 second delay
+import time
+
+# Ask the user to enter a number to start counting down from
+number = int(input("Enter a number to start the countdown: "))
+
+# Keep looping as long as the number is greater than 0
+while number > 0:
+    print(number)  # Print the current number
+    time.sleep(1) #Wait for 1 second before continuing
+    number -= 1    # Subtract 1 from number each time
+
+# Once the loop ends, print a message
+print("🎉 Time's up!")
