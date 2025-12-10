b06abb7142230dd9e7fc4d8287a3ea01854a1672
twilightprincessisthebestgameever <iluvskywardsword@gmail.com>
2025-12-09 18:05:23 -0600

    Update number.py

--- number.py
+++ number.py
@@ -1,16 +1,19 @@
-#Program Description: The purpose of this program is to reverse the number entered by the user
+# Program Description:
+# This program takes a word as input from the user and prints the word in reverse order.
+# It uses a while loop to iterate from the last character to the first.
 
+# Ask the user to enter a word
+s = input("Enter a word: ")
+rev = ""
 
-# Take the number from the user as a string (so we can access each digit easily)
-n = input("Enter a number: ")
+# Start index at the last character of the string (len(s) - 1)
+i = len(s) - 1
 
-rev = ""            # This will store the reversed number
-i = len(n) - 1      # Start from the last index of the string
-
-# Loop backward through the string
+# Loop while index is greater than or equal to 0
 while i >= 0:
-    rev = rev + n[i]   # Add the current character to the reversed string
-    i -= 1             # Move to the previous character
+    rev = rev + s[i] # Add the current character to the reversed string 
+    # Move to the previous character
+    i -= 1
 
 # Display the reversed result
 print(f"The number reversed is : {rev}")
